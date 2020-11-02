ab143701617741c88c09bbbb4c5e840a068a4a35
Oldrich <oldrich@lqd.cz>
2020-11-02 10:11:56 +0100

    fixed

--- src/SqlGenerator/Column.php
+++ src/SqlGenerator/Column.php
@@ -120,7 +120,7 @@ class Column implements ISqlEntity
 		$type = $this->column->getType();
 		$length = $this->column->getLength() ? '('. $this->column->getLength() .')' : '';
 		$nullable = $this->column->isNullable() ? ' NULL' : ' NOT NULL';
-		$noWrap = \is_numeric($this->column->getDefault()) || $type === 'enum' || $this->column->getDefault() === 'CURRENT_TIMESTAMP';
+		$noWrap = \is_numeric($this->column->getDefault()) || $this->column->getDefault() === 'CURRENT_TIMESTAMP';
 		$default = $this->column->getDefault() !== null ? ' DEFAULT ' . ($noWrap ? $this->column->getDefault() : "'".$this->column->getDefault()."'") : '';
 		$extra = $this->column->isAutoincrement() ? ' AUTO_INCREMENT' : ($this->column->getExtra() ? ' ' . $this->column->getExtra() : '');
 		$comment = $this->column->getComment() ? ' COMMENT ' . $this->connection->quote($this->column->getComment()) : '';
